dd4fca0c0e34941127dbc3fb23e56f79c1419f2b
Julian <julian_delisio@hotmail.com>
2022-02-19 23:59:00 -0300

    creación del server y rutas

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.set("view engine", "ejs");
 app.use("/", require("./router"));
 
 app.listen(5000, () => {
-    console.log("Server corriendo en http://localhost:5000");
+    console.log("Server running in http://localhost:5000");
 });
